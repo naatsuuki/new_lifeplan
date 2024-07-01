a7ee7eab3cb66155109e97f433ae9fe8150a73dd
Natsuki <natsuki.sayama@gmail.com>
2024-07-01 14:49:06 +0900

    自分以外の人の登録

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,22 +1,7 @@
-# app/controllers/people_controller.rb
 class PeopleController < ApplicationController
-  def index
-    @people = Person.all
+  def new
   end
 
   def create
-    @person = Person.new(person_params)
-
-    if @person.save
-      redirect_to welcome_index_path, notice: '登録しました。'
-    else
-      render 'welcome/index'
-    end
-  end
-
-  private
-
-  def person_params
-    params.require(:person).permit(:name, :birth_year)
   end
 end
