2eca0718489af3e6bb1619f53998987530d2b142
Natsuki <natsuki.sayama@gmail.com>
2024-07-01 12:34:43 +0900

    ユーザー追加機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new  # 新しいユーザーオブジェクトを作成してビューに渡す
+  def index
+    @users = User.all
   end
 
-  def index
-    @users = User.all  # すべてのユーザーを取得してビューに渡す
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      redirect_to root_path, alert: 'Failed to create user.'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :birth_year)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :create, :destroy]  # createアクションを追加
   resources :people, only: [:new, :create]
-  resources :users, only: [:new, :create, :index]  # 必要なアクションのみを指定することを推奨します
+
   get 'welcome/index'
-  get '/signup', to: 'users#new'  # 新規登録ページへのルート
-  get '/login', to: 'sessions#new'  # ログインページへのルート
-  post '/login', to: 'sessions#create'  # ログインフォームの送信処理
-  delete '/logout', to: 'sessions#destroy'  # ログアウト処理
-  root 'welcome#index'  # ルートパスの指定（必要に応じて変更）
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  root 'welcome#index'
 end
