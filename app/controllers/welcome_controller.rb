7d203d54df31589536eb03041c1d65a1ae24df66
Natsuki <natsuki.sayama@gmail.com>
2024-07-02 12:04:56 +0900

    ログイン機能の追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
+# app/controllers/welcome_controller.rb
 class WelcomeController < ApplicationController
   def index
     @users = User.all
+    @user = User.new  # 追加: 新規ユーザーのフォーム用インスタンス
   end
 end
