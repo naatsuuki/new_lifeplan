d019f0fd6a5d089753a0397b996bdd9c6809df5a
Natsuki <natsuki.sayama@gmail.com>
2024-07-02 12:12:41 +0900

    ログイン機能の追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
+# app/helpers/sessions_helper.rb
+
 module SessionsHelper
+  # 現在のユーザーがログインしているかどうかを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # セッションに記録されているユーザーを返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
+  
